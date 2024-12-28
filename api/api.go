aa8a879bba481bdc46ae3263a729f50094244c43
Whadislov <morissejulien76110@gmail.com>
2024-12-28 23:15:23 +0100

    WIP : trying to build an API

--- api/api.go
+++ api/api.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"log"
+	"net/http"
+)
+
+func RunApi() {
+	http.HandleFunc("/users", GetUsers)
+	http.HandleFunc("/users/create", CreateUser)
+	http.HandleFunc("/users/get", GetUser)
+	http.HandleFunc("/users/update", UpdateUser)
+	http.HandleFunc("/users/delete", DeleteUser)
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ api/models.go
@@ -0,0 +1,11 @@
+package api
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []User{
+	{ID: 1, Name: "John Doe"},
+	{ID: 2, Name: "Jane Smith"},
+}
