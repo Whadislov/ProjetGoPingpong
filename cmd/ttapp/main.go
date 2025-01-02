64bfe97ddbd7d90673299a85be408711999687ce
Whadislov <morissejulien76110@gmail.com>
2025-01-02 11:27:50 +0100

    integration of the config file management bloc

--- cmd/ttapp/main.go
+++ cmd/ttapp/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Whadislov/ProjetGoPingPong/api"
 	msql "github.com/Whadislov/ProjetGoPingPong/internal/my_sqlitedb"
 	mu "github.com/Whadislov/ProjetGoPingPong/internal/my_ui"
 	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
 )
 
 func main() {
+	// Load the configuration file
+	config, err := api.LoadConfig("config.json")
+	if err != nil {
+		log.Printf("Error loading config: %v", err)
+	}
 
 	// Load the database (deserialize)
 	golangDB, err := msql.LoadDB()
@@ -15,6 +23,7 @@ func main() {
 		fmt.Println("Error while loading golang database:", err)
 		return
 	}
+	go api.RunApi(golangDB, config)
 	mu.Display(golangDB)
 
 }
